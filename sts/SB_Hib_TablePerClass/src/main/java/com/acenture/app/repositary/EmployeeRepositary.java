dd2bfb13d3494aff73d754fe5e33280caebaa659
Amit Patil <amitjipatil@gmail.com>
2024-10-18 18:20:19 +0530

    Delete sts directory

--- sts/SB_Hib_TablePerClass/src/main/java/com/acenture/app/repositary/EmployeeRepositary.java
+++
@@ -1,9 +0,0 @@
-package com.acenture.app.repositary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.acenture.app.domain.Employee;
-
-public interface EmployeeRepositary extends JpaRepository<Employee,Integer> {
-
-}
